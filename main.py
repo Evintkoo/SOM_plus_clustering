79a2dc38320d3aabc785fa674c71577be234fffb
Evintkoo <99308658+Evintkoo@users.noreply.github.com>
2024-04-11 00:48:11 -0700

    optimize initialization

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import time
 if __name__ == "__main__":
     df = pd.read_csv("data.csv", header=None).iloc[:, :-1]
     X = df.values
-    m1 = 10
-    m2 = 10
-    n1 = 10
-    n2 = 10
+    m1 = 2
+    m2 = 2
+    n1 = 2
+    n2 = 2
     model1 = SOM(m=m1,n=n1,dim=X.shape[1], initiate_method="random", neighbour_rad=0.1,learning_rate=0.1,distance_function="euclidean")
     cur = time.time()
     model1.fit_predict(X, 10, verbose=False)
