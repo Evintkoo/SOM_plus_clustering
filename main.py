b81e65a0b2f47af91147647b39239f34f0831ba8
Evintkoo <99308658+Evintkoo@users.noreply.github.com>
2024-04-26 17:03:42 -0700

    add main.py

--- main.py
+++ main.py
@@ -6,29 +6,31 @@ import time
 if __name__ == "__main__":
     df = pd.read_csv("data.csv", header=None).iloc[:, :-1]
     X = df.values
-    m = 3
-    n = 4
+    m = 10
+    n = 10
     init1 = "random"
     init2 = "SOM++"
     init3 = "kde"
+    epochs = 100
+    
     model1 = SOM(m=m,n=n,dim=X.shape[1], initiate_method=init1, neighbour_rad=0.1,learning_rate=0.1,distance_function="euclidean")
     cur = time.time()
-    model1.fit_predict(X, 10, verbose=False)
+    model1.fit_predict(X, epochs, verbose=False)
     ex_time = time.time() - cur
-    model2 = SOM(m=m,n=n,dim=X.shape[1], initiate_method=init2, neighbour_rad=0.1,learning_rate=0.1,distance_function="cosine")
+    model2 = SOM(m=m,n=n,dim=X.shape[1], initiate_method=init2, neighbour_rad=0.1,learning_rate=0.1,distance_function="euclidean")
     cur = time.time()
-    model2.fit_predict(X, 10, verbose=False)
+    model2.fit_predict(X, epochs, verbose=False)
     ex_time2 = time.time() - cur
-    model3 = SOM(m=m,n=n,dim=X.shape[1], initiate_method=init3, neighbour_rad=0.1,learning_rate=0.1,distance_function="cosine")
+    #model3 = SOM(m=m,n=n,dim=X.shape[1], initiate_method=init3, neighbour_rad=0.1,learning_rate=0.1,distance_function="euclidean")
     cur = time.time()
-    model3.fit_predict(X, 10, verbose=False)
+    #model3.fit_predict(X, 10, verbose=False)
     ex_time3 = time.time() - cur
     print("time for", init1, "neurons:", ex_time)
     print("time for", init2, "neurons", ex_time2)
     print("time for", init3, "neurons", ex_time3)
     print("time difference:", (ex_time2)/ex_time*100 - 100,"%")
-    print("time difference:", (ex_time3)/ex_time*100 - 100,"%")
+    #print("time difference:", (ex_time3)/ex_time*100 - 100,"%")
     print("eval for model 1")
     print(model1.evaluate(X=X, method="all"))
     print(model2.evaluate(X=X, method="all"))
-    print(model3.evaluate(X=X, method="all"))
+    #print(model3.evaluate(X=X, method="all"))
